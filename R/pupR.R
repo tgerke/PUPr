079d70b7e8710450c8fb8105e3e4a032a50b8b70
Travis Gerke <tgerke@hsph.harvard.edu>
2019-02-01 10:49:10 -0500

    modify PUPr.R to get PUP tracks instead of doggos

--- R/pupR.R
+++ R/pupR.R
@@ -1,46 +1,43 @@
 #' pupR
 #'
-#' Your daily dose of doggo!
+#' Your daily dose of PUP!
 #'
-#' @return A photo of a dog is downloaded and displayed in the plot window.
+#' @return A random track from PUP is served to you to click in the Console
 #'
-#' @importFrom rvest html_session html_nodes html_attr
-#' @importFrom imager load.image
-#' @importFrom stringr str_split str_remove_all
+#' @importFrom rvest read_html html_nodes html_attr
+#' @importFrom stringr str_detect
 #'
 #' @examples
-#' pupR()
+#' PUPr()
 #'
 #' @export
-pupR <- function(){
+PUPr <- function(){
+  library(rvest)
+  library(stringr)
 
-  # This is where all the doggos are
-  dogpark <- html_session('https://pixabay.com/en/photos/dog/?image_type=photo')
+  # parse album the dream is over and get track links
+  dream <- read_html("https://puptheband.bandcamp.com/album/the-dream-is-over")
+  urls <- dream %>%
+    html_nodes("a") %>%
+    html_attr("href")
+  dreamtracks <- urls[which(str_detect(urls, 'track') & !str_detect(urls, 'action'))] %>%
+    unique() %>%
+    str_c("https://puptheband.bandcamp.com", .)
 
-  # Play hide and seek with the doggos
-  hiddendoggos <- html_nodes(dogpark, "img")
+  # parse album pup and get track links
+  pup <- read_html("https://puptheband.bandcamp.com/album/pup")
+  urls <- pup %>%
+    html_nodes("a") %>%
+    html_attr("href")
+  puptracks <- urls[which(str_detect(urls, 'track') & !str_detect(urls, 'action'))] %>%
+    unique() %>%
+    str_c("https://puptheband.bandcamp.com", .)
 
-  # Found them!
-  nothiddendoggos <- html_attr(hiddendoggos,"src")
-  nothiddendoggos <- nothiddendoggos[-grep("static", nothiddendoggos)]
+  # add the new track
+  kids <- "https://puptheband.bandcamp.com/track/kids"
 
-  # Wait... We are missing some
-  extrasneakydoggos <- html_attr(hiddendoggos,"data-lazy-srcset")
-  extrasneakydoggos <- str_split(na.omit(str_remove_all(extrasneakydoggos,"[1-9]x")),", ")
-  extrasneakydoggos <- unlist(extrasneakydoggos)
-
-  # Round them all up
-  goodboyz <- c(nothiddendoggos, extrasneakydoggos)
-
-  # Take them home
-  temporary_file_location <- paste0(tempdir(), "/doggo.png")
-  download.file(goodboyz[sample(1:length(goodboyz),1)],
-                temporary_file_location, mode = "wb")
-
-  # Bring the doggo to R
-  doggo <- load.image(temporary_file_location)
-
-  # Display the doggo
-  plot(doggo, yaxt = 'n', axes = FALSE)
+  # concatenate into the sampling vector
+  urls <- c(dreamtracks, puptracks, kids)
+  print(sample(urls, size = 1))
 }
 
